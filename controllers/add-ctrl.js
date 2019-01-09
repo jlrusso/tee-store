b4ffb53d41689206881602b728999a8bc6416ca1
James Russo <1jlrusso@gmail.com>
2019-01-09 15:37:04 -0800

    attach .then() and .catch() to promise returned by addProduct()

--- controllers/add-ctrl.js
+++ controllers/add-ctrl.js
@@ -6,12 +6,15 @@ exports.goToAdd = (req, res) => {
     path: '/add'
   });
 }
+
 exports.addProduct = (req, res) => {
-  console.log('works');
   const title = req.body.title;
   const image = req.body.image;
-  const price = `$${req.body.price}`;
-  const product = new Product(title, image, price)
-  product.addProduct();
-  res.redirect('/home');
+  const price = `${req.body.price}`;
+  const product = new Product(title, image, price);
+  product.addProduct().then(() => {
+    res.redirect('/home');
+  }).catch(err => {
+    console.log(err);
+  })
 }
