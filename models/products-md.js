db561d86d73421452dbcbce9cab66e6679234721
James Russo <1jlrusso@gmail.com>
2019-01-09 15:39:26 -0800

    require connection to database and update methods to execute queries

--- models/products-md.js
+++ models/products-md.js
@@ -1,48 +1,21 @@
-const path = require('path');
-const fs = require('fs');
 const uniqid = require('uniqid');
-
-const pathToFile = path.join(__dirname, '../data', 'products.json');
-
-const getProductsFromFile = callback => {
-  fs.readFile(pathToFile, (err, content) => {
-    if(err){
-      return callback([]);
-    } else {
-      return callback(JSON.parse(content));
-    }
-  });
-}
+const db = require('../util/database');
 
 module.exports = class Product {
-  constructor(name, image, price){
-    this.name = name;
+  constructor(title, image, price){
+    this.title = title;
     this.image = image;
     this.price = price;
   }
   addProduct(){
-    this.id = uniqid();
-    getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(pathToFile, JSON.stringify(products, null, 2), err => {
-        console.log(err);
-      });
-    });
+    return db.execute(`INSERT INTO products (title, price, image) VALUES (?, ?, ?)`, 
+      [this.title, this.price, this.image] 
+    );
   }
-  static getProductDetails(productId, callback){
-    getProductsFromFile(products => {
-      const product = products.find(prod => prod.id === productId);
-      callback(product);
-    });
+  static getProductDetails(productTitle){
+    return db.execute('SELECT * FROM products WHERE title=?', [productTitle]);
   }
-  static editProduct(productId){
-    getProductsFromFile(() => {
-
-    });
-  }
-  static fetchProducts(callback){
-    getProductsFromFile(products => {
-      callback(products);
-    });
+  static fetchProducts(){
+    return db.execute('SELECT * FROM products');
   }
 }
