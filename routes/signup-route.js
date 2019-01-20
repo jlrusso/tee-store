6704db1d15186505015bf22a960688f7e2ed8a54
James Russo <1jlrusso@gmail.com>
2019-01-19 22:29:27 -0800

    first commit of signup route

---
+++ routes/signup-route.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const signupCtrl = require('../controllers/signup-ctrl');
+const errorCtrl = require('../controllers/error-ctrl');
+
+const redirectHome = (req, res, next) => {
+  if(req.session.isLoggedIn){
+    res.redirect('/home');
+  } else {
+    next();
+  }
+}
+
+router.get('/', redirectHome, signupCtrl.goToSignup);
+router.post('/', redirectHome, signupCtrl.userAuth);
+router.use(errorCtrl.goToError);
+
+module.exports = router;
