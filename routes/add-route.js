d6c36d7dcb9c4480ab36515fe4f2e00c86ec9174
James Russo <1jlrusso@gmail.com>
2019-01-19 23:04:39 -0800

    implement error controller catch and middleware to check login status

--- routes/add-route.js
+++ routes/add-route.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const addCtrl = require('../controllers/add-ctrl');
+const errorCtrl = require('../controllers/error-ctrl');
 
-router.get('/', addCtrl.goToAdd);
-router.post('/new-product', addCtrl.addProduct);
+const redirectHome = (req, res, next) => {
+  if(!req.session.isLoggedIn){
+    res.redirect('/home');
+  } else {
+    next();
+  }
+}
+
+router.get('/', redirectHome, addCtrl.goToAdd);
+router.post('/new-product', redirectHome, addCtrl.addProduct);
+router.use(errorCtrl.goToError);
 
 module.exports = router;
