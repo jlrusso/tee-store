29fae036ea85bb084cf829461dc9d8c10b47e8a3
James Russo <1jlrusso@gmail.com>
2019-01-19 23:13:59 -0800

    pass req.session values to render()

--- controllers/home-ctrl.js
+++ controllers/home-ctrl.js
@@ -1,6 +1,9 @@
 exports.displayHome = (req, res) => {
+  const username = (req.session.user) ? req.session.user.username : null;
   res.render('home', {
     pageTitle: 'TeeStore | Home',
+    isLoggedIn: req.session.isLoggedIn,
+    username: username,
     path: '/home'
   });
 }
