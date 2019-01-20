800996855c8ecf31b2f8b67674f10271441b37c8
James Russo <1jlrusso@gmail.com>
2019-01-19 23:15:00 -0800

    reconfigure model with sequelize

--- models/cart-md.js
+++ models/cart-md.js
@@ -1,49 +1,13 @@
-const path = require('path');
-const fs = require('fs');
+const Sequelize = require('sequelize');
+const dbconn = require('../util/database');
 
-const pathToFile = path.join(__dirname, '../data', 'cart.json');
-
-const getCartProducts = callback => {
-  fs.readFile(pathToFile, (err, content) => {
-    if(err) return callback([]);
-    return callback(JSON.parse(content));
-  });
-}
-
-const checkExistingThenAdd = (product, callback) => {
-  getCartProducts(products => {
-    const newCartProducts = [...products];
-    const prodIndex = newCartProducts.findIndex(prod => prod.id === product.id);
-    if(prodIndex !== -1){
-      newCartProducts[prodIndex].quantity = parseInt(newCartProducts[prodIndex].quantity) + 1;
-      callback(newCartProducts);
-    } else {
-      const newProduct = {...product};
-      newCartProducts.push(newProduct);
-      callback(newCartProducts);
-    }
-  });
-}
-
-module.exports = class Cart {
-  static addProduct(product){
-    checkExistingThenAdd(product, cartProducts => {
-      fs.writeFile(pathToFile, JSON.stringify(cartProducts, null, 2), err => {
-        console.log(err);
-      });
-    });
+const Cart = dbconn.define('cart', {
+  id: {
+    type: Sequelize.UUID,
+    primaryKey: true,
+    allowNull: false,
+    defaultValue: Sequelize.UUIDV4
   }
-  static removeProduct(productId){
-    getCartProducts(cartProducts => {
-      const newCart = cartProducts.filter(prod => prod.id !== productId);
-      fs.writeFile(pathToFile, JSON.stringify(newCart, null, 2), err => {
-        console.log(err);
-      });
-    });
-  }
-  static fetchCart(callback){
-    getCartProducts(cartProducts => {
-      callback(cartProducts)
-    });
-  }
-}
+});
+
+module.exports = Cart;
