1ad4bc6d310a21087373e88b6849c77e2b9d75ab
James Russo <1jlrusso@gmail.com>
2019-01-19 23:03:31 -0800

    implement error controller and middleware that checks login status

--- routes/cart-route.js
+++ routes/cart-route.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const cartCtrl = require('../controllers/cart-ctrl');
+const errorCtrl = require('../controllers/error-ctrl');
 
-router.post('/add-product', cartCtrl.addToCart);
-router.get('/remove-product/:productId', cartCtrl.removeProduct);
-router.get('/', cartCtrl.goToCart);
+const redirectHome = (req, res, next) => {
+  if(!req.session.isLoggedIn){
+    res.redirect('/home');
+  } else {
+    next();
+  }
+}
+
+router.get('/', redirectHome, cartCtrl.goToCart);
+router.post('/add-product', redirectHome, cartCtrl.addToCart);
+router.get('/remove-product/:productId', redirectHome, cartCtrl.removeProduct);
+router.get('/checkout', redirectHome, cartCtrl.goToCheckout);
+router.use(errorCtrl.goToError);
 
 module.exports = router;
