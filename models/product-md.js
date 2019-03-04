e173a28acf62174f6bfc1a524390c95800f83fad
James Russo <1jlrusso@gmail.com>
2019-03-03 18:58:21 -0800

    reconfig model as class-based with mongodb database connection

--- models/product-md.js
+++ models/product-md.js
@@ -1,29 +1,54 @@
-const Sequelize = require('sequelize');
-const dbconn = require('../util/database');
+const { ObjectId } = require("mongodb");
+const { getDb } = require("../util/database");
 
-const Product = dbconn.define('product', {
-  id: {
-    type: Sequelize.UUID,
-    allowNull: false,
-    primaryKey: true,
-    defaultValue: Sequelize.UUIDV4
-  },
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  front_image: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  back_image: {
-    type: Sequelize.STRING,
-    allowNull: false
+class Product {
+  constructor(title, description, frontImg, backImg, price){
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.frontImg = frontImg;
+    this.backImg = backImg;
   }
-});
+  create(){
+    const db = getDb();
+    return db.collection("products").insertOne(this)
+    .then(result => {
+      console.log(result);
+    }).catch(err => console.log(err));
+  }
+  static fetch(prodId){
+    const db = getDb();
+    return db.collection("products").findOne({
+      _id: new ObjectId(prodId)
+    }).then(product => product)
+    .catch(err => console.log(err));
+  }
+  static fetchSelect(prodIds, cart){
+    const db = getDb();
+    return db.collection("products").find({_id: {$in: prodIds}}).toArray()
+    .then(products => ({products, cart}))
+    .catch(err => console.log(err));
+  }
+  static fetchAll(){
+    const db = getDb();
+    return db.collection("products").find().toArray()
+    .then(products => products)
+    .catch(() => console.log("no products"));
+  }
+  static delete(prodId){
+    const db = getDb();
+    return db.collection("products").deleteOne({
+      _id: new ObjectId(prodId)
+    }).then(() => console.log('product deleted'))
+    .catch(() => console.log('product not deleted'));
+  }
+  static update(prodId, product){
+    const db = getDb();
+    return db.collection("products").updateOne({
+      _id: new ObjectId(prodId)
+    }, {$set: product}).then(() => console.log('product updated'))
+    .catch(() => console.log('product not updated'));
+  }
+}
 
 module.exports = Product;
