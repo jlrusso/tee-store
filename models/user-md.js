14205974560656d40751309e7210c42e0e1330ee
James Russo <1jlrusso@gmail.com>
2019-03-03 18:50:33 -0800

    reconfig user model as class-based with mongodb

--- models/user-md.js
+++ models/user-md.js
@@ -1,25 +1,41 @@
-const Sequelize = require('sequelize');
-const dbconn = require('../util/database');
+const { getDb } = require("../util/database");
+const Cart = require("../models/cart-md");
 
-const User = dbconn.define('user', {
-  id: {
-    type: Sequelize.UUID,
-    primaryKey: true,
-    allowNull: false,
-    defaultValue: Sequelize.UUIDV4
-  },
-  username: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  password: {
-    type: Sequelize.STRING,
-    allowNull: false
+class User {
+  constructor(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.admin = false;
   }
-});
+  create(){
+    const db = getDb();
+    return db.collection("users").insertOne(this)
+    .then(user => Promise.resolve(user)).then(user => {
+      const newCart = new Cart(user._id);
+      newCart.create();
+      return user;
+    }).catch(err => console.log(err));
+  }
+  static fetch({username, password}){
+    const db = getDb();
+    return db.collection("users").findOne({username, password})
+    .then(user => user)
+    .catch(err => console.log(err));
+  }
+  static checkExisting({username, email}){
+    const db = getDb();
+    return db.collection("users").findOne({
+      $or: [{username, email}, {email}]
+    }).then(user => user)
+    .catch(err => console.log(err));
+  }
+  static update(userId, cart){
+    const db = getDb();
+    return db.collection("users").updateOne({_id: new ObjectId(userId)}, {$set: {cart}})
+    .then(() => console.log("cart updated"))
+    .catch(() => console.log("cart not updated"));
+  }
+}
 
 module.exports = User;
