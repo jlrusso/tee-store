4eba57bc57cada6d2355a8c1762ed59bed95ecf5
James Russo <1jlrusso@gmail.com>
2019-03-03 18:44:29 -0800

    add checkUserAuth method, update routes for editing/deleting products

--- routes/products-route.js
+++ routes/products-route.js
@@ -3,8 +3,19 @@ const router = express.Router();
 const productCtrl = require('../controllers/product-ctrl');
 const errorCtrl = require('../controllers/error-ctrl');
 
+const checkUserAuth = (req, res, next) => {
+  if(!req.session.isLoggedIn){
+    res.redirect('/home');
+  } else {
+    next();
+  }
+}
+
 router.get('/', productCtrl.goToProducts);
-router.get('/product-details/:productTitle', productCtrl.goToDetails);
+router.get('/delete/:prodId', checkUserAuth, productCtrl.deleteProduct);
+router.get("/edit-product/:prodId", checkUserAuth, productCtrl.goToEdit);
+router.post("/edit-product/:prodId", checkUserAuth, productCtrl.editProduct);
+router.get('/product-details/:prodId', productCtrl.goToDetails);
 router.use(errorCtrl.goToError);
 
 module.exports = router;
