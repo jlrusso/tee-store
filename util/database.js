ea122bfec42b784fca16690953d44c053fea650a
James Russo <1jlrusso@gmail.com>
2019-03-03 18:31:46 -0800

    reconfig database connection with MongoDb

--- util/database.js
+++ util/database.js
@@ -1,9 +1,24 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+const uri = "mongodb+srv://<USERNAME>:<PASSWORD>@firstcluster-ydhgw.mongodb.net/shop?retryWrites=true";
+const client = new MongoClient(uri, {useNewUrlParser: true});
+let db;
 
-const dbconn = new Sequelize('tee-store', 'root', 'password', {
-  dialect: 'mysql',
-  host: 'localhost',
-  port: 3307
-});
+const connectClient = callback => {
+  client.connect()
+  .then(() => {
+    console.log("NoSQL Connected");
+    db = client.db();
+    callback();
+  }).catch(err => console.log(err));
+};
 
-module.exports = dbconn;
+const getDb = () => {
+  if(db){
+    return db;
+  }
+  throw 'database not found';
+};
+
+exports.connectClient = connectClient;
+exports.getDb = getDb;
