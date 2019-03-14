e046e7495af73958a537fa3bf6d12e7b9c709a63
James Russo <1jlrusso@gmail.com>
2019-03-13 17:06:36 -0700

    desctructure req.session to add props to object passed to render()

--- controllers/home-ctrl.js
+++ controllers/home-ctrl.js
@@ -1,11 +1,13 @@
 exports.displayHome = (req, res) => {
   if(req.session.user){
+    const { user, isLoggedIn, cartNumber } = req.session;
     res.render('home', {
       pageTitle: 'TeeStore | Home',
-      username: req.session.user.username,
-      isLoggedIn: req.session.isLoggedIn,
-      admin: req.session.user.admin,
-      id: req.session.user.id,
+      cartNumber: cartNumber,
+      username: user.username,
+      isLoggedIn: isLoggedIn,
+      admin: user.admin,
+      id: user.id,
       path: '/home'
     });
   } else {
