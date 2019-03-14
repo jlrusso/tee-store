4a0c566efd92da652fd58b25277dc3c3ea4962b5
James Russo <1jlrusso@gmail.com>
2019-03-13 17:02:41 -0700

    destructure req.session to add username, cartNumber, isLoggedIn, and admin

--- controllers/add-ctrl.js
+++ controllers/add-ctrl.js
@@ -2,11 +2,13 @@ const Product = require('../models/product-md');
 
 exports.goToAdd = (req, res) => {
   if(req.session.user){
+    const { user, isLoggedIn, cartNumber } = req.session;
     res.render('add', {
       pageTitle: 'TeeStore | Add',
-      username: req.session.user.username,
-      isLoggedIn: req.session.isLoggedIn,
-      admin: req.session.user.admin,
+      username: user.username,
+      cartNumber: cartNumber,
+      isLoggedIn: isLoggedIn,
+      admin: user.admin,
       path: '/add'
     });
   } else {
