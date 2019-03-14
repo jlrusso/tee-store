ee6d794cb001951074e46c621de510839ba6f513
James Russo <1jlrusso@gmail.com>
2019-03-13 19:08:58 -0700

    spread itemPriceFields and upate on quantity change, add function to set cart subtotal

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,32 +1,46 @@
 (function(){
   const reduceBtns = document.getElementsByClassName('reduce-btn');
   const increaseBtns = document.getElementsByClassName('increase-btn');
-  const quantityFields = document.querySelectorAll('.item-quantity-wrap input');
-  const itemPriceFields = document.querySelectorAll('.item-price-wrap > p');
+  const quantityFields = document.querySelectorAll('.cart-item-quantity-wrap input');
+  const itemPriceFields = document.querySelectorAll('.cart-item-price');
+  const cartSubtotal = document.getElementById('cart-subtotal');
   
-  [...reduceBtns].forEach((btn, index) => {
-    updateItemPrice(index);
-    btn.addEventListener('click', reduceQuantityHandler.bind(null, index));
-  });
-  [...increaseBtns].forEach((btn, index) => {
-    btn.addEventListener('click', increaseQuantityHandler.bind(null, index));
-  });
-  
-  function reduceQuantityHandler(index){
+  const reduceItemQuantity = index => {
     if(quantityFields[index].value > 1){
       quantityFields[index].value = parseInt(quantityFields[index].value) - 1;
-      updateItemPrice(index);
+      updateItemPrices();
     }
-  }
-  function increaseQuantityHandler(index){
+  };
+
+  const increaseItemQuantity = index => {
     if(quantityFields[index].value < 99) {
       quantityFields[index].value = parseInt(quantityFields[index].value) + 1;
-      updateItemPrice(index);
+      updateItemPrices();
     }
-  }
-  function updateItemPrice(index){
-    const itemPrice = itemPriceFields[index].getAttribute('data-price');
-    const itemTotal = (quantityFields[index].value * parseFloat(itemPrice)).toFixed(2);
-    itemPriceFields[index].textContent = `$ ${itemTotal}`;
-  }
+  };
+
+  const setCartSubtotal = () => {
+    const subtotalVal = [...itemPriceFields].reduce((acc, field) => {
+      return acc + parseFloat(field.textContent.replace('$ ', ''));
+    }, 0);
+    cartSubtotal.textContent = `$ ${subtotalVal.toFixed(2)}`;
+  };
+
+  const updateItemPrices = () => {
+    [...itemPriceFields].forEach((field, index) => {
+      const itemPrice = field.getAttribute('data-price');
+      const itemTotal = (quantityFields[index].value * parseFloat(itemPrice)).toFixed(2);
+      field.textContent = `$ ${itemTotal}`;
+    });
+    setCartSubtotal();
+  };
+  updateItemPrices();
+
+  [...reduceBtns].forEach((btn, index) => {
+    btn.addEventListener('click', reduceItemQuantity.bind(null, index));
+  });
+  [...increaseBtns].forEach((btn, index) => {
+    btn.addEventListener('click', increaseItemQuantity.bind(null, index));
+  });
+
 }());
