e81e2e35b375e0b393ca724f9ae39a28d7967a88
James Russo <1jlrusso@gmail.com>
2019-03-13 19:06:36 -0700

    check user auth with imported 'authCheck' functions

--- routes/cart-route.js
+++ routes/cart-route.js
@@ -2,19 +2,12 @@ const express = require('express');
 const router = express.Router();
 const cartCtrl = require('../controllers/cart-ctrl');
 const errorCtrl = require('../controllers/error-ctrl');
+const authCheck = require('../middleware/auth-check');
 
-const redirectHome = (req, res, next) => {
-  if(!req.session.isLoggedIn){
-    res.redirect('/home');
-  } else {
-    next();
-  }
-}
-
-router.get('/', redirectHome, cartCtrl.goToCart);
-router.post('/add-product', redirectHome, cartCtrl.addProduct);
-router.get('/remove-product/:productId', redirectHome, cartCtrl.removeProduct);
-router.get('/checkout', redirectHome, cartCtrl.goToCheckout);
+router.get('/', authCheck.isAuth, cartCtrl.goToCart);
+router.post('/add-product', authCheck.isAuth, cartCtrl.addProduct);
+router.get('/remove-product/:productId', authCheck.isAuth, cartCtrl.removeProduct);
+router.get('/checkout', authCheck.isAuth, cartCtrl.goToCheckout);
 router.use(errorCtrl.goToError);
 
 module.exports = router;
